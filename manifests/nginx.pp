57545f26c7ff1f23d53ed212c19105dfdac38e81
root <root@adonyec1.mylabserver.com>
2018-08-27 20:59:02 +0000

    use hieradata

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -4,9 +4,9 @@ class profile::nginx (
   include ::nginx
 
   $vhosts.each |$vhost| {
-    ::nginx::vhosts { $vhost['fqdn']:
-    port           => $vhost['port'],
-    server_aliases => $vhost['server_aliases'],
+    ::nginx::vhost { $vhost['fqdn']:
+      port           => $vhost['port'],
+      server_aliases => $vhost['server_aliases'],
     }
   }
 }
